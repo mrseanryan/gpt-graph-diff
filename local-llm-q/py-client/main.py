793ee910bef8e6f964ce9b13671efe67da4468c6
Sean Ryan <str_ie@yahoo.co.uk>
2023-11-02 16:30:49 +0100

    Add basic Python client

---
+++ local-llm-q/py-client/main.py
@@ -0,0 +1,14 @@
+from gradio_client import Client
+
+import config
+
+client = Client(config.URL)
+
+def send_prompt(prompt):
+    print(">> " + prompt)
+    return client.predict(
+            prompt,	# str  in 'Message' Textbox component
+            api_name="/chat"
+    )
+
+print(send_prompt("Howdy!"))
